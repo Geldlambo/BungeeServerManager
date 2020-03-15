a6cbb6dab84ba93b720af3a4737dcfa6f1d887b7
NxtUserHD <50436997+NxtUser-HD@users.noreply.github.com>
2020-03-15 19:34:55 +0100

    Update ServerManagerCommand.java

--- src/main/java/pw/teg/bsm/commands/ServerManagerCommand.java
+++ src/main/java/pw/teg/bsm/commands/ServerManagerCommand.java
@@ -406,7 +406,7 @@ public class ServerManagerCommand extends Command {
         sender.sendMessage(getHelpString("/svm edit " + serverName + " motd <motd>", "Change this server's motd"));
     }
 
-    private SocketAddress getIp(String input) {
+    private InetSocketAddress getIp(String input) {
         if (!input.contains(":") || !input.contains(".")) {
             return null;
         }
@@ -433,6 +433,6 @@ public class ServerManagerCommand extends Command {
             }
         }
 
-        return new SocketAddress(parts[0], Integer.valueOf(parts[1]));
+        return new InetSocketAddress(parts[0], Integer.valueOf(parts[1]));
     }
 }
